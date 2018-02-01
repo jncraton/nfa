76b0c28d04ae0815d750c2b597747f016440104c
Jon Craton <jncraton@gmail.com>
2018-01-31 19:54:09 -0500

    Refactored to DFA class

--- homework/1/9.py
+++ homework/1/9.py
@@ -1,62 +1,95 @@
-from string import ascii_lowercase, digits
+from string import ascii_lowercase as lower, digits
 from collections import ChainMap
 
-# This is a relatively human-readable defintion of the DFA as
-# (initial state, characters to match, next state)
-# This will be used to build the formal definition
-dfa = [
-  ('start', ascii_lowercase+digits, 'username'),
-  ('username', ascii_lowercase+digits, 'username'),
-  ('username', '@', '@'),
-  ('@', ascii_lowercase+digits, 'domain'),
-  ('domain', ascii_lowercase+digits, 'domain'),
-  ('domain', '.', 'tld length 0'),
-  ('tld length 0', ascii_lowercase+digits, 'tld length 1'),
-  ('tld length 1', ascii_lowercase+digits, 'tld length 2'),
-  ('tld length 1', '.', 'tld length 0'),
-  ('tld length 2', ascii_lowercase+digits, 'tld length 3'),
-  ('tld length 2', '.', 'tld length 0'),
-  ('tld length 3', ascii_lowercase+digits, 'domain'),
-  ('tld length 3', '.', 'tld length 0'),
-]
-
-Q = set([i[0] for i in dfa] + [i[2] for i in dfa])
-Σ = set(sum([[c for c in i[1]] for i in dfa], []))
-δ = dict(ChainMap(*[{(i[0], char): i[2] for char in i[1]} for i in dfa]))
-q0 = dfa[0][0]
-F = ['tld length 2', 'tld length 3']
-
-def check_input(input, state=q0):
+class DFA:
   """
-  Recursively check input one character at a time
-
-  Returns True when halted in a state in F, and False otherwise
-
-  >>> check_input('abc@dsu.edu')
-  True
-  >>> check_input('abc@pluto.dsu.edu')
-  True
-  >>> check_input('11@123.com')
-  True
-  >>> check_input('a.b.ab')
-  False
-  >>> check_input('ab@ab')
-  False
-  >>> check_input('ab@ab.abcd')
-  False
+  Implements a Deterministic Finite Automaton
   """
+  
+  def __init__(self, transitions, accept=None):
+    """
+    The DFA is constructed by a list of transitions of the form
+            [
+                (initial state, symbols, next state),
+                (initial state, symbols, next state),
+                ...
+            ]
+      
+    The 5 formal attributes of the DFA 5-tuple are built from this.
+    
+    An optional set of accept states may be provided. If omitted, the
+    last state listed is assumed to be the one and only accept state.
+    """
 
-  #print(state, input)
+    # Set of states Q
+    self.Q = set([i[0] for i in transitions] + [i[2] for i in transitions])
 
-  # If we have no more input, return our result
-  if len(input) == 0:
-    return state in F
+    # Symbol alphabet Σ
+    self.Σ = set(sum([[c for c in i[1]] for i in transitions], []))
 
-  # Use our transition function to get the next state
-  try:
-    next = δ[(state,input[0])]
-  except KeyError:
-    return state in F
+    # Transition function relating state and symbol to another state 
+    # δ: Q × Σ → Q
+    self.δ = dict(ChainMap(*[{(i[0], char): i[2] for char in i[1]} for i in transitions]))
 
-  # Recurse using our input less the character we just processed and our next state
-  return check_input(input[1:], next)
+    # Start state qₒ
+    self.qₒ = transitions[0][0]
+    
+    # Accept states F
+    self.F = accept or transitions[-1][2]
+
+
+  def accept(self, input, state=None):
+    """
+    Recursively check input for acceptance one symbol at a time
+  
+    Returns True when halted at a state in F, and False otherwise
+    """
+    
+    # Begin at the start state if we aren't called with a state
+    state = state or self.qₒ
+  
+    # Return our acceptance status when we're out of input
+    if len(input) == 0:
+      return state in self.F
+  
+    # Get the next state or return our acceptance status if we're stuck
+    try:
+      next = self.δ[(state,input[0])]
+    except KeyError:
+      return state in self.F
+  
+    # Recurse using our next state and the rest of our input
+    return self.accept(input[1:], next)
+
+  @classmethod
+  def email_validator(cls):
+    """
+    >>> ev = DFA.email_validator()
+    >>> ev.accept('abc@dsu.edu')
+    True
+    >>> ev.accept('abc@pluto.dsu.edu')
+    True
+    >>> ev.accept('11@123.com')
+    True
+    >>> ev.accept('a.b.ab')
+    False
+    >>> ev.accept('ab@ab')
+    False
+    >>> ev.accept('ab@ab.abcd')
+    False
+    """
+    return cls([
+      ('start', lower+digits, 'username'),
+      ('username', lower+digits, 'username'),
+      ('username', '@', '@'),
+      ('@', lower+digits, 'domain'),
+      ('domain', lower+digits, 'domain'),
+      ('domain', '.', 'tld length 0'),
+      ('tld length 0', lower+digits, 'tld length 1'),
+      ('tld length 1', lower+digits, 'tld length 2'),
+      ('tld length 1', '.', 'tld length 0'),
+      ('tld length 2', lower+digits, 'tld length 3'),
+      ('tld length 2', '.', 'tld length 0'),
+      ('tld length 3', lower+digits, 'domain'),
+      ('tld length 3', '.', 'tld length 0'),
+    ], ['tld length 2', 'tld length 3'])
